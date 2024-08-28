64431ddefcb5fddeb4e3d7e7ecac80d78340ac88
Waheed KhAn <waheedkhan6446@gmail.com>
2024-08-28 15:29:52 +0500

    Add dice_roller class as StatefulWidget

--- lib/gradient_containter.dart
+++ lib/gradient_containter.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/styled_text.dart';
+import 'package:roll_dice/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-// ignore: must_be_immutable
 class GradientContainer extends StatelessWidget {
-  GradientContainer({super.key, required this.colors});
+  const GradientContainer({super.key, required this.colors});
 
   final List<Color> colors;
-  var activeDiceImage = 'assets/images/dice-1.png';
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-2.png';
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,24 +15,8 @@ class GradientContainer extends StatelessWidget {
       decoration: BoxDecoration(
           gradient: LinearGradient(
               colors: colors, begin: startAlignment, end: endAlignment)),
-      child: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Image.asset(
-            activeDiceImage,
-            width: 200,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(fontSize: 28),
-            ),
-            child: const StyledText("Roll Dice"),
-          ),
-        ]),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
